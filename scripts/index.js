5915eab569ca51c106c432646196c4bce54ba38e
asha <ashabadavagol1668@gmail.com>
2023-07-09 13:09:48 +0530

    add file

--- scripts/index.js
+++ scripts/index.js
@@ -1,78 +1,101 @@
-async function getData(){
-    try{
-     let api = await fetch(`https://fakestoreapi.com/products`);
-     let data  = await api.json();
-      append(data);
-     console.log("data", data);
+async function getData()
+{
+  let api = await fetch(`https://fakestoreapi.com/products`)
+  let data = await api.json();
+  console.log("data",data)
+}
+getData()
+
+
+let todoArray = JSON.parse(localStorage.getItem("todos")) || [];
+appendTodo();
+
+
+let handleClick = () => {
+  // alert("iam clicked")
+
+  let value = document.querySelector("#todo_input").value
+
+  if (!value) {
+    alert("Type something");
+    return;
+  }
+
+  let payload = {
+    todo: value,
+    status: false,
+    id: Date.now(), value
+  }
+  todoArray.push(payload);
+  localStorage.setItem("todos", JSON.stringify(todoArray));
+  // window.location.reload();
+  appendTodo();
+
+}
+
+// id:Date.now(),value  ----solt----
+function appendTodo() {
+  let container = document.querySelector("#all_tododiv");
+  container.innerHTML = null;
+  todoArray.map((el, i) => {
+
+    let mainDiv = document.createElement("div");
+    let todoDiv = document.createElement("div");
+    let toggleDiv = document.createElement("div");
+    let deleteDiv = document.createElement("div");
+
+    let todoH3 = document.createElement("h3");
+    let toggleButton = document.createElement("button");
+    let deleteButton = document.createElement("button");
+
+    todoH3.innerText = el.todo;
+
+    if (el.status) {
+      toggleButton.innerText = "Done";
+      toggleButton.style.backgroundColor = "green"
     }
-    catch{
-     console.log("err")
-     let div = document.querySelector("#product__div");
-     let h1 = document.createElement("h1");
-     h1.textContent = "some error from server"
-     div.append(h1)
-  }
-  }
-  getData();
-  
-  function append(data){
-     let container = document.querySelector("#product__div");
-     container.innerHTML = null;
-     data.map((el)=>{
-         //html element
-         let mainDiv = document.createElement("div");
-         let imageDiv = document.createElement("div")
-         let contentDiv = document.createElement("div")
-         let img = document.createElement("img")
-         let category = document.createElement("p");
-         let price = document.createElement("p");
-         let buttonDiv = document.createElement("div");
-         let button = document.createElement("button");
-         let cartButton = document.createElement("button");
-         //setattribute
-         img.src = el.image
-         category.innerText = el.category
-         price.innerText  = el.price
-         button.innerText = "BUY";
-         cartButton.innerText = "Add To Cart";
-  
-         cartButton.style.backgroundColor = "teal"
-         button.style.backgroundColor = "red"
-         mainDiv.style.border = "1px solid white";
-         mainDiv.style.paddingBottom = "1rem"
-  
-         //event listner
-         // button.addEventListener("click" , byButtononclick)
-  
-         imageDiv.append(img)
-         buttonDiv.append(button, cartButton)
-         contentDiv.append(category, price, buttonDiv)
-         mainDiv.append(imageDiv, contentDiv)
-         //append
-       container.append(mainDiv)
-  
-     })
-  }
-  
-  // function byButtononclick(){
-  
-  //     // alert("buy")
-  //     setTimeout(() =>{
-  //         alert("successfully add to cart")
-  //         setTimeout(() =>{
-  //             alert("payment done");
-  //             setTimeout(() =>{
-  //                 alert("order delivered")
-  //             },2000)
-  //         },3000)
-  //     },2000)
-  // }
-  
-  // let Uname=localStorage.getItem("name")
-  // if(Uname==null){
-  //     document.getElementById("usrr").innerText=``
-  // }else{
-  //     document.getElementById("usrr").innerText=`Hii  ${Uname}`
-  // }
-  
-  
+    else {
+      toggleButton.innerText = "NotDone";
+      toggleButton.style.backgroundColor = "red"
+
+    }
+    deleteButton.innerText = "Delete";
+
+    toggleButton.addEventListener("click", () =>{
+      updateTodo(el.id);
+    }) 
+    deleteButton.addEventListener("click", () =>{
+      deleteTodo(el.id);
+    })
+
+
+    todoDiv.append(todoH3);
+    toggleDiv.append(toggleButton);
+    deleteDiv.append(deleteButton);
+    mainDiv.append(todoDiv, toggleDiv, deleteDiv);
+    container.append(mainDiv);
+  })
+}
+
+function updateTodo(id){
+   todoArray = todoArray.map((el,i) =>{
+   if(el.id === id){
+    return{ ...el, status:!el.status, }
+   }
+   else{
+    return el;
+   }
+  })
+localStorage.setItem("todos", JSON.stringify(todoArray))
+appendTodo()
+  console.log(todoArray);
+}
+
+function deleteTodo(id)
+{
+  todoArray = todoArray.filter((el) =>{
+    return el.id != id
+  })
+  localStorage.setItem("todos", JSON.stringify(todoArray))
+appendTodo()
+}
